568f0aaeb301c6b590a53285ff82d78b5cbdf501
kgkjvlll <kgkjvlll@gmail.com>
2022-09-20 14:33:12 +0800

    add(peach): OCR识别

---
+++ peach.py
@@ -0,0 +1,33 @@
+import os, cv2
+
+img_path = "jpg\\"# 这里放原始文件
+del_path = "del\\"# 这里是需要删除的文件
+use_path = "use\\"# 这里是需要OCR的文件
+restart = True
+cv2.namedWindow("image", 0)
+# cv2.resizeWindow("image", 1080, 1080)
+while restart:
+    restart = False
+    vp = os.listdir(img_path)
+    for path in vp:
+        img = cv2.imread(img_path + path)
+        cv2.imshow('image', img)
+        des = cv2.waitKey(0)
+        # print(des)
+        if des == 49:# 按1保存
+            os.replace(img_path + path, use_path + path)
+            origin_temp = img_path + path
+            path_temp = use_path + path
+        elif des == 50:# 按2删除
+            os.replace(img_path + path, del_path + path)
+            origin_temp = img_path + path
+            path_temp = del_path + path
+        elif des == 32:# 按空格查看上一个文件
+            os.replace(path_temp, origin_temp)
+            restart = True
+            break
+
+        elif des == 113:# 按q退出
+            break
+
+# if __name__ == '__main__':
